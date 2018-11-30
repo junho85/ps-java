efe0b25be3221ed88e0038da8f8bde0e40a1a066
june.kim <june.kim@kakaocorp.com>
2018-11-30 23:32:34 +0900

    leetcode 1. Two Sum. hashmap 으로 개선 assertj 테스트 코드 작성

--- src/main/java/leetcode/PS1TwoSum.java
+++ src/main/java/leetcode/PS1TwoSum.java
@@ -1,10 +1,44 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
+    /**
+     * hashmap 으로 개선
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        Map<Integer, Integer> mapNums = new HashMap<>();
+
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            mapNums.put(nums[i], i);
+        }
+
+        for (int i = 0; i < length; i++) {
+            int findNum = target - nums[i];
+            Integer foundIndex = mapNums.get(findNum);
+            if (foundIndex != null && foundIndex != i) {
+                result[0] = i;
+                result[1] = foundIndex;
+                return result;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * ArrayList 를 이용한 방식. 검색이 빠르지 않다.
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum_slow(int[] nums, int target) {
         int[] result = new int[2];
 
         List<Integer> listNums = new ArrayList<>();
@@ -14,7 +48,7 @@ class Solution {
         }
 
         int length = listNums.size();
-        for (int i=0; i<listNums.size(); i++) {
+        for (int i=0; i<length; i++) {
             Integer num = listNums.get(i);
             List<Integer> subList = listNums.subList(i + 1, length);
 
@@ -22,27 +56,10 @@ class Solution {
             if (index != -1) {
                 result[0] = i;
                 result[1] = index + i + 1;
+                return result;
             }
         }
 
         return result;
-    }
-}
-
-public class PS1TwoSum {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        int[] nums = new int[]{2,7,11,15};
-        int[] result = solution.twoSum(nums, 9);
-        System.out.println(result[0]);
-        System.out.println(result[1]);
-        System.out.println();
-
-        nums = new int[]{3,2,4};
-        result = solution.twoSum(nums, 6);
-        System.out.println(result[0]);
-        System.out.println(result[1]);
-        System.out.println();
     }
 }
