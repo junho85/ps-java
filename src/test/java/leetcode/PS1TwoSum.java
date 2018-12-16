add874448007a223c07aa74de3dd9a9603a8ae2a
june.kim <june.kim@kakaocorp.com>
2018-12-16 23:04:20 +0900

    leetcode 1 - simplify code

--- src/test/java/leetcode/PS1TwoSum.java
+++ src/test/java/leetcode/PS1TwoSum.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class PS1TwoSumTest {
+public class PS1TwoSum {
 
     class Solution {
         /**
@@ -79,20 +79,9 @@ public class PS1TwoSumTest {
     public void twoSum() {
         Solution solution = new Solution();
 
-        int[] nums = new int[]{2,7,11,15};
-        int[] result = solution.twoSum(nums, 9);
-        assertThat(result).contains(0).contains(1);
-
-        nums = new int[]{3,2,4};
-        result = solution.twoSum(nums, 6);
-        assertThat(result).contains(1).contains(2);
-
-        nums = new int[]{3,3};
-        result = solution.twoSum(nums, 6);
-        assertThat(result).contains(0).contains(1);
-
-        nums = new int[]{2,5,5,11};
-        result = solution.twoSum(nums, 10);
-        assertThat(result).contains(1).contains(2);
+        assertThat(solution.twoSum(new int[]{2,7,11,15}, 9)).contains(0).contains(1);
+        assertThat(solution.twoSum(new int[]{3,2,4}, 6)).contains(1).contains(2);
+        assertThat(solution.twoSum(new int[]{3,3}, 6)).contains(0).contains(1);
+        assertThat(solution.twoSum(new int[]{2,5,5,11}, 10)).contains(1).contains(2);
     }
 }
