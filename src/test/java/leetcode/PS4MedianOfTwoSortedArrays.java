cde5012221b5af0fc0f748be5d61bd8b5aa07a38
june.kim <june.kim@kakaocorp.com>
2018-12-16 22:02:13 +0900

    leetcode 4. Median of Two Sorted Arrays

---
+++ src/test/java/leetcode/PS4MedianOfTwoSortedArrays.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PS4MedianOfTwoSortedArrays {
+
+    class Solution {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+            // merge two arrays
+            int[] merged = new int[nums1.length + nums2.length];
+            System.arraycopy(nums1, 0, merged, 0, nums1.length);
+            System.arraycopy(nums2, 0, merged, nums1.length, nums2.length);
+
+            Arrays.sort(merged);
+
+            int center = merged.length/2;
+            if (merged.length % 2 == 0) {
+                return (double)(merged[center-1] + merged[center])/2;
+            } else {
+                return merged[center];
+            }
+        }
+    }
+
+    @Test
+    public void addTwoNumbers() {
+        Solution solution = new Solution();
+
+        assertThat(solution.findMedianSortedArrays(new int[]{1,3}, new int[]{2})).isEqualTo(2.0);
+        assertThat(solution.findMedianSortedArrays(new int[]{1,2}, new int[]{3,4})).isEqualTo(2.5);
+    }
+}
